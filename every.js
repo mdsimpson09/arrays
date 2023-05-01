1b8a422cb7af7210d9785883b0a641ee18d5b905
mdsimpson09 <131899003+mdsimpson09@users.noreply.github.com>
2023-05-01 13:26:03 -0400

    reduce

--- every.js
+++ every.js
@@ -76,3 +76,53 @@ return arr.find(function(name){
     return name.username === username; 
 });
 }
+
+/////REDUCE 
+
+// Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
+
+function extraValues(arr, key){
+    return arr.reduce(function(accu, next){
+    accu.push(next[key]);
+    }, []);
+}
+
+// Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
+
+function vowelCount(string){
+    let vowel = 'aeiou';
+    return string.split(' ').reduce(function(acc, next){
+       let lowerCase = next.toLowerCase()
+        if(vowel.indexOf(lowerCase) !== -1){
+            if(acc[lowerCase]){
+                acc[lowercase]++;
+            } else {
+                acc[lowerCase] = 1;
+            }
+        }
+        return acc;
+    }, {});
+}
+
+
+// Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
+
+function addKeyAndValue(arr, key, val){
+    return arr.reduce(function(acc, next, index){
+        acc[index][key] = val;
+        return acc;  
+    }, arr);
+}
+
+// Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray.
+
+function partition(arr, callback){
+    return arr.reduce(function(acc, next){
+    if(callback(next)){
+        acc[0].push(next);
+    } else{
+        acc[1].push(next);
+    }
+    return acc;
+    }[[],[]]);
+}
