dad0c58b6e44fd9cf2fe27473a54f52a64cb407a
mdsimpson09 <131899003+mdsimpson09@users.noreply.github.com>
2023-05-01 12:14:40 -0400

    find

--- every.js
+++ every.js
@@ -58,3 +58,21 @@ function hasCertainValue(arr, key, value){
         return val[key] === value;
     });
 }
+
+///FIND
+
+// Write a function called removeUser which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array and return this object. If the object is not found, return undefined.
+function removeUser(arr, username){
+    let foundIndex = arr.findIndex(function(name){
+    return name.username === username;
+    });
+    if(foundIndex === -1) return;
+    return arr.splice(foundIndex, 1)[0];
+
+// Write a function called findUserByUsername which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined.
+
+function findUserByName(arr, username){
+return arr.find(function(name){
+    return name.username === username; 
+});
+}
